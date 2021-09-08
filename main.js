05c80be277afb69316d51d93fe994e420c5eaf7c
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-09-08 17:40:26 +0200

    Replace content of main__question in index.html with js function renderCard and const questionsObj

--- main.js
+++ main.js
@@ -62,7 +62,8 @@ buttonElHome.addEventListener('click', () => {
 const questionsObj = [
   {
     question: 'To which famous masterpiece does this detail belong?',
-    img: '/images/arnolfinidetail.png',
+    imgDetail: '/images/arnolfinidetail.png',
+    imgFull: '/images/arnolfini.png',
     answer:
       "The image detail shown above is a portion of the painting <strong>'The Arnolfini Wedding'</strong> by <strong>Jan van Eyck</strong> from <strong>1434</strong>. The artwork is located in the <strong>National Gallery in London</strong>.",
     isBookmarked: false,
@@ -71,7 +72,8 @@ const questionsObj = [
   },
   {
     question: 'To which famous masterpiece does this detail belong?',
-    img: '/images/starrynightdetail.png',
+    imgDetail: '/images/starrynightdetail.png',
+    imgFull: '/images/starrynight.png',
     answer:
       "The moon can be seen in the oil painting <strong>'The Starry Night'</strong> by <strong>Vincent van Gogh</strong>. Van Gogh painted the picture in <strong>June 1889</strong> and it can be seen today in the <strong>Museum of Modern Art in New York</strong>.",
     isBookmarked: false,
@@ -80,7 +82,8 @@ const questionsObj = [
   },
   {
     question: 'To which famous masterpiece does this detail belong?',
-    img: 'images/goyadetail.png',
+    imgDetail: '/images/goyadetail.png',
+    imgFull: '/images/goya.png',
     answer:
       "<strong>Francisco Goya</strong> painted <strong>'The Third of May'</strong> in the year <strong>1814</strong>. It's also known as 'El tres mayo de 1808en Madrid' and is located in the <strong>Museo del Prado in Madrid</strong>.",
     isBookmarked: false,
@@ -89,7 +92,8 @@ const questionsObj = [
   },
   {
     question: 'To which famous masterpiece does this detail belong?',
-    img: 'images/wandererdetail.png',
+    imgDetail: '/images/wandererdetail.png',
+    imgFull: '/images/wanderer.png',
     answer:
       "The <strong>'Wanderer above the Sea of Fog'</strong> was painted by <strong>Caspar David Friedrich</strong>. It' estimated that it was created <strong>around 1808</strong>. Today it is on display in the <strong>Kunsthalle Hamburg</strong>.",
     isBookmarked: false,
@@ -98,7 +102,8 @@ const questionsObj = [
   },
   {
     question: 'To which famous masterpiece does this detail belong?',
-    img: 'images/andrewsdetail.png',
+    imgDetail: '/images/andrewsdetail.png',
+    imgFull: '/images/andrews.png',
     answer:
       "The hand belongs to a portrait of Mrs Andrews in <strong>'Mr and Mrs Andrews'</strong> by <strong>Thomas Gainsborough</strong>. It's located in the <strong>National Gallery in London</strong> and was painted <strong>around 1750</strong>.",
     isBookmarked: false,
@@ -106,6 +111,97 @@ const questionsObj = [
     tags: ['19th century', 'painting', 'England'],
   },
 ]
+
+// Question cards
+
+function renderCard(questionsObj) {
+  const cardSection = document.createElement('section')
+  cardSection.classList.add('card')
+  document.getElementById('main__questions').appendChild(cardSection)
+
+  const cardHeader = document.createElement('div')
+  cardHeader.classList.add('card-header')
+  cardSection.appendChild(cardHeader)
+
+  const cardTitle = document.createElement('h2')
+  cardTitle.classList.add('card-header__title')
+  cardTitle.textContent = 'Questions:'
+  cardHeader.appendChild(cardTitle)
+
+  const cardBookmark = document.createElement('button')
+  cardBookmark.classList.add('question-bookmark')
+  cardBookmark.classList.add('card-header__bookmark')
+  cardBookmark.setAttribute('aria-label', 'Bookmark')
+  cardHeader.appendChild(cardBookmark)
+
+  const cardContent = document.createElement('div')
+  cardContent.classList.add('card-content')
+  cardSection.appendChild(cardContent)
+
+  const cardContentQuestion = document.createElement('p')
+  cardContentQuestion.classList.add('card-content__question')
+  cardContentQuestion.textContent = questionsObj.question
+  cardContent.appendChild(cardContentQuestion)
+
+  const questionImage = document.createElement('img')
+  questionImage.classList.add('card-content__img')
+  questionImage.classList.add('img-select')
+  questionImage.setAttribute('aria-label', 'Detail of masterpiece for question')
+  questionImage.setAttribute('src', questionsObj.imgDetail)
+  cardContent.appendChild(questionImage)
+
+  const cardButtonElAnswer = document.createElement('button')
+  cardButtonElAnswer.classList.add('card-content__show-answer')
+  cardButtonElAnswer.classList.add('answer-button')
+  cardButtonElAnswer.textContent = 'Show answer'
+  cardContent.appendChild(cardButtonElAnswer)
+
+  const cardContentAnswer = document.createElement('div')
+  cardContentAnswer.classList.add('answer')
+  cardContentAnswer.classList.add('answer--hidden')
+  cardContentAnswer.classList.add('present-answer')
+  cardContent.appendChild(cardContentAnswer)
+
+  const answerHeader = document.createElement('div')
+  answerHeader.classList.add('answer-header')
+  cardContentAnswer.appendChild(answerHeader)
+
+  const answerTitle = document.createElement('h3')
+  answerTitle.classList.add('answer__title')
+  answerTitle.textContent = 'Answer:'
+  answerHeader.appendChild(answerTitle)
+
+  const answerContent = document.createElement('div')
+  answerContent.classList.add('answer__content')
+  cardContentAnswer.appendChild(answerContent)
+
+  const answerImage = document.createElement('img')
+  answerImage.classList.add('content__img')
+  answerImage.classList.add('img-select')
+  answerImage.setAttribute('aria-label', 'Full view of masterpiece')
+  answerImage.setAttribute('src', questionsObj.imgFull)
+  answerContent.appendChild(answerImage)
+
+  const answerText = document.createElement('p')
+  answerText.classList.add('content__text')
+  answerText.innerHTML = questionsObj.answer
+  answerContent.appendChild(answerText)
+
+  const tagsList = document.createElement('ul')
+  tagsList.classList.add('tags')
+  cardSection.appendChild(tagsList)
+
+  questionsObj.tags.forEach(tag => {
+    const tagsItem = document.createElement('li')
+    tagsItem.classList.add('tags__item')
+    tagsItem.textContent = tag
+    tagsList.appendChild(tagsItem)
+  })
+}
+
+questionsObj.forEach(object => {
+  renderCard(object)
+})
 
 // Bookmarks
 
