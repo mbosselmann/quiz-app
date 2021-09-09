10720d3b441a2d2f8a88967ad2ae748c837c0276
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-09-09 17:09:00 +0200

    Current changes before restructuring of js file

--- main.js
+++ main.js
@@ -158,24 +158,23 @@ function renderCards(questionsObj, id) {
 
 // add question
 
-const newQuestion = {
-  question: 'To which famous masterpiece does this detail belong?',
-  imgDetail: '/images/andrewsdetail.png',
-  imgFull: '/images/andrews.png',
-  answer:
-    "The hand belongs to a portrait of Mrs Andrews in <strong>'Mr and Mrs Andrews'</strong> by <strong>Thomas Gainsborough</strong>. It's located in the <strong>National Gallery in London</strong> and was painted <strong>around 1750</strong>.",
-  isBookmarked: false,
-  showAnswer: false,
-  tags: ['19th century', 'painting', 'England'],
-}
+function addQuestion() {
+  const newQuestion = {
+    question: 'To which famous masterpiece does this detail belong?',
+    imgDetail: '/images/andrewsdetail.png',
+    imgFull: '/images/andrews.png',
+    answer:
+      "The hand belongs to a portrait of Mrs Andrews in <strong>'Mr and Mrs Andrews'</strong> by <strong>Thomas Gainsborough</strong>. It's located in the <strong>National Gallery in London</strong> and was painted <strong>around 1750</strong>.",
+    isBookmarked: false,
+    showAnswer: false,
+    tags: ['19th century', 'painting', 'England'],
+  }
 
-function addQuestion(question) {
-  questionsObj.push(question)
-}
+  questionsObj.push(newQuestion)
 
-addQuestion(newQuestion)
-console.table(questionsObj)
-console.log(questionsObj)
+  console.table(questionsObj)
+  console.log(questionsObj)
+}
 
 questionsObj.forEach(object => {
   renderCards(object, 'main__questions')
@@ -272,6 +271,7 @@ const resetFormElButton = document.querySelector(
 
 resetFormElAfterSubmitButton.addEventListener('click', () => {
   document.getElementById('form').reset()
+  addQuestion()
   result = window.confirm(
     'Your question has been successfully submitted and will be reviewed at some point in the future and once successfully reviewed will be included with the already existing questions. Thank you for your efforts! :-)'
   )
